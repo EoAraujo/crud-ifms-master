5ae8bc6d1ab85e2166a587038dd3585f5561404c
EoAraujo <eo.araujo@outlook.com>
2022-04-12 00:07:39 -0300

    Commit

---
+++ src/main/java/br/edu/ifms/crudspring/services/ProfessorService.java
@@ -0,0 +1,38 @@
+package br.edu.ifms.crudspring.services;
+
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.ifms.crudspring.model.Professor;
+import br.edu.ifms.crudspring.repositories.ProfessorRepository;
+
+
+
+public class ProfessorService {
+    
+    @Autowired
+    ProfessorRepository repository;
+ 
+    public List<Professor> getProfessor(){  
+        return  repository.findAll();
+    }  
+        
+     public void save(Professor professor){
+         this.repository.save(professor);
+     }   
+  
+     public void delete(UUID id){
+         repository.deleteById(id);    
+     }
+
+     public Professor findById(UUID id){  
+         return repository.findById(id).get(); 
+     }
+
+     public void updateProfessor(Professor professor){ 
+          repository.save(professor);     
+     }
+}
